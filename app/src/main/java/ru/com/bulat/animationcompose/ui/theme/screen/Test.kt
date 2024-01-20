8ee46ca2c14ead3af89d260b6a889da07a352c48
VBulat74 <Vladimir.Bulat@gmail.com>
2024-01-20 19:12:14 +0300

    Lesson_09_08

--- app/src/main/java/ru/com/bulat/animationcompose/ui/theme/screen/Test.kt
+++ app/src/main/java/ru/com/bulat/animationcompose/ui/theme/screen/Test.kt
@@ -1,6 +1,11 @@
 package ru.com.bulat.animationcompose.ui.theme.screen
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +18,16 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,74 +41,116 @@ fun Test() {
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var isIncreased by remember {
+            mutableStateOf(true)
+        }
+
+        val size by animateDpAsState(targetValue = if (isIncreased) 200.dp else 100.dp)
+
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {isIncreased = !isIncreased}
         ) {
             Text(
                 text = "Animate size",
             )
         }
         AnimatedContainer(
-            text = "Size"
+            text = "Size",
+            size = size
         )
+
+        var isRounded by remember {
+            mutableStateOf(false)
+        }
+        val radius by animateIntAsState(targetValue = if (isRounded) 50 else 4)
+
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {isRounded = !isRounded}
         ) {
             Text(
                 text = "Animate shape",
             )
         }
         AnimatedContainer(
-            text = "Shape"
+            text = "Shape",
+            radiusPercent = radius
         )
+
+        var isSelected by remember {
+            mutableStateOf(false)
+        }
+        val borderWith by animateDpAsState(targetValue = if (isSelected) 16.dp else 0.dp)
+
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {isSelected = !isSelected}
         ) {
             Text(
                 text = "Animate border",
             )
         }
         AnimatedContainer(
-            text = "Border"
+            text = "Border",
+            borderWith = borderWith,
         )
+
+        var isChanged by remember {
+            mutableStateOf(false)
+        }
+        val color by animateColorAsState(targetValue = if (isChanged) Color.Red else Color.Blue)
+
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {isChanged = !isChanged}
         ) {
             Text(
                 text = "Animate color",
             )
         }
         AnimatedContainer(
-            text = "Color"
+            text = "Color",
+            color = color
         )
+
+        var isVisibility by remember {
+            mutableStateOf(true)
+        }
+        val alpha by animateFloatAsState(targetValue = if (isVisibility) 1f else 0f)
+
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {isVisibility = !isVisibility}
         ) {
             Text(
                 text = "Animate visibility",
             )
         }
         AnimatedContainer(
-            text = "Visibility"
+            text = "Visibility",
+            alpha = alpha
         )
     }
 }
 
 @Composable
 private fun AnimatedContainer(
-    text: String
+    text: String,
+    size : Dp = 200.dp,
+    radiusPercent :Int = 4,
+    borderWith : Dp = 0.dp,
+    color : Color = Color.Blue,
+    alpha : Float = 1f,
 ) {
     Box(
         modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(Color.Blue)
-            .size(200.dp),
-        contentAlignment = Alignment.Center
+            .alpha(alpha = alpha)
+            .clip(RoundedCornerShape(radiusPercent))
+            .border(width = borderWith, color = Color.Red)
+            .background(color)
+            .size(size),
+        contentAlignment = Alignment.Center,
     ) {
         Text(
             text = text,
